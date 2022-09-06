6a35e04044a664a531acfe3f4bd2b159f1c021a5
Jenna Hakulinen <jennash@k-d557-mac-32.pk.amk.root>
2022-09-06 11:48:46 +0300

    chat aloitus

---
+++ server/public/chat.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const socket = io("http://localhost:3000");
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inp = document.getElementById("message");
+  console.log("emitting:", inp.value);
+  socket.emit("chat message", inp.value);
+  inp.value = "";
+});
+
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li");
+  item.innerHTML = msg;
+  document.getElementById("messages").appendChild(item);
+});
