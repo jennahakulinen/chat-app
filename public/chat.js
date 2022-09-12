f5771d6456baf6769e959a030722ff6364ba3cc5
jennahakulinen <jenna.hakulinen@metropolia.fi>
2022-09-12 15:52:48 +0300

    tailwind

--- public/chat.js
+++ public/chat.js
@@ -1,15 +1,17 @@
 "use strict";
 
-// const socket = io("http://localhost:3000");
-const socket = io("https://stream-server-jennash.norwayeast.cloudapp.azure.com");
+const socket = io("http://localhost:3000");
+// const socket = io("https://stream-server-jennash.norwayeast.cloudapp.azure.com");
 
-document.querySelector("#chat-message-input").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const inp = document.getElementById("message");
-  console.log("emitting:", inp.value);
-  socket.emit("chat message", inp.value);
-  inp.value = "";
-});
+document
+  .querySelector("#chat-message-input")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inp = document.getElementById("message");
+    console.log("emitting:", inp.value);
+    socket.emit("chat message", inp.value);
+    inp.value = "";
+  });
 document.querySelector("#join").addEventListener("submit", (event) => {
   event.preventDefault();
   const inp = document.getElementById("username");
@@ -21,6 +23,15 @@ document.querySelector("#join").addEventListener("submit", (event) => {
 
 socket.on("chat message", (msg) => {
   const item = document.createElement("li");
+  item.classList.add(
+    "py-2",
+    "px-3",
+    "bg-mm-medium-carmine",
+    "rounded-lg",
+    "text-white",
+    "w-fit",
+    "m-5"
+  );
   item.innerHTML = msg;
   document.getElementById("messages").appendChild(item);
 });
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        "mm-charcoal": "rgba(64, 76, 83, 1)",
+        "mm-alice-blue": "rgba(227, 235, 238, 1)",
+        "mm-cool-grey": "rgba(135, 146, 166, 1)",
+        "mm-jet": "rgba(53, 54, 51, 1)",
+        "mm-medium-carmine": "rgba(177, 65, 46, 1)",
+        "mm-medium-carmine-opacity": "rgba(177, 65, 46, 0.75)",
+      },
+    },
+  },
+  plugins: [],
+};
